47365762f4a1f91739ca24b6f49cede75bcac05c
Avijeet Kartikay <avi.kartikay@gmail.com>
2019-04-21 00:31:28 -0400

    My codes
    Playground codes and one primary code to combine two midi files

---
+++ code/combine.py
@@ -0,0 +1,23 @@
+import music21 as ms
+
+def combine(left, right, filepath='test.mid'):
+    """
+    Input: left - the stream of notes played by the 
+                pianist with his left hand
+            right - the stream of notes played by the 
+                pianist with his right hand
+            filepath - path to the file in which o/p
+                midi stream is to be stored
+    Output: N/A
+
+    The combines the streams from the left stream and right 
+    stream and outputs a single stream object
+    """
+    sc = ms.stream.Stream()
+    sc.append(right)
+    sc.append(left)
+    left.offset = 0.0
+    if save:
+        sc.write("midi", filepath)
+    else:
+        return sc       
